de337dc43db60a32add32939b32f42ef9eb4a398
Nate Dalo <ndalo@hey.com>
2022-12-03 08:37:59 -0600

    day 3 complete

---
+++ 2022/day3/3-1.rb
@@ -0,0 +1,29 @@
+# part 1
+rucksacks = open('./input3.txt').read.split("\n")
+
+sum = 0
+lower_chars = *('a'..'z')
+upper_chars = *('A'..'Z')
+numbers = *(1..52)
+mapping = (lower_chars + upper_chars).zip(numbers).to_h
+
+rucksacks.each do |rucksack|
+  left, right = rucksack.chars.each_slice(rucksack.size/2).to_a
+  intersection = left.intersection(right)
+  total = intersection.map { |letter| mapping[letter] }.sum
+  sum += total
+end
+
+puts sum
+
+# part 2
+sum = 0
+
+rucksacks.each_slice(3).to_a.each do |group|
+  group = group.map(&:chars)
+  intersection = group[0].intersection(group[1], group[2])
+  total = intersection.map { |letter| mapping[letter] }.sum
+  sum += total
+end
+
+puts sum
