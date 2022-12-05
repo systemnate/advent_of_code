1a92264547916c11fd31fe02be3d94bb6b0bd511
Nate Dalo <ndalo@hey.com>
2022-12-05 10:08:35 -0600

    add back day 1

---
+++ 2022/day1/1.rb
@@ -0,0 +1,14 @@
+# Part 1
+puts open('./input1.txt').read.split("\n\n")
+  .map { |data| data.split("\n") }
+  .map { |data| data.map(&:to_i) }
+  .map(&:sum)
+  .max
+
+# Part 2
+puts open('./input1.txt').read.split("\n\n")
+  .map { |data| data.split("\n") }
+  .map { |data| data.map(&:to_i) }
+  .map(&:sum)
+  .max(3)
+  .sum
