7fe2eab37e9141908079539a7e8c88ffa959ca0d
Nate Dalo <ndalo@hey.com>
2022-12-08 23:30:50 -0600

    finish day 8-2

--- 2022/day8/8.rb
+++ 2022/day8/8.rb
@@ -19,12 +19,64 @@ puts "outside_edges: #{outside_edges}"
 
 inner_edges = 0
 
+scenic_score = 0
+scenic_scores = []
 input[1..-2].each_with_index do |row, i|
   row[1..-2].each_with_index do |char, j|
     top_rows = input.transpose[j + 1][0..i]
     bottom_rows = input.transpose[j + 1][i+2..]
     left_rows = input[i + 1][0..j]
     right_rows = input[i + 1][j + 2..]
+
+    # trip = rand(1..9000)
+    # if trip == 3731
+    #   binding.pry
+    # end
+
+    top_score, right_score, bottom_score, left_score = 0, 0, 0, 0
+
+    left_rows.reverse.each do |n|
+      if char > n
+        left_score += 1
+      end
+      if char <= n
+        left_score += 1
+        break
+      end
+    end
+
+    right_rows.each do |n|
+      if char > n
+        right_score += 1
+      end
+      if char <= n
+        right_score += 1
+        break
+      end
+    end
+
+    top_rows.reverse.each do |n|
+      if char > n
+        top_score += 1
+      end
+      if char <= n
+        top_score += 1
+        break
+      end
+    end
+
+    bottom_rows.each do |n|
+      if char > n
+        bottom_score += 1
+      end
+      if char <= n
+        bottom_score += 1
+        break
+      end
+    end
+
+    scenic_score = top_score * right_score * bottom_score * left_score
+    scenic_scores << scenic_score
 
     if char > top_rows.max || char > bottom_rows.max || char > left_rows.max || char > right_rows.max
       inner_edges += 1
@@ -34,3 +86,7 @@ end
 
 puts "inner_edges: #{inner_edges}"
 puts "total: #{outside_edges + inner_edges}"
+# 1030750 too high
+# 606441 too high
+p scenic_scores.count
+p scenic_scores.max
