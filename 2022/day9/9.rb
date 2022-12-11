8ee63a2130c31809db18f8630f818282e09a7640
Nate Dalo <ndalo@hey.com>
2022-12-11 11:55:06 -0600

    remove debugging code

--- 2022/day9/9.rb
+++ 2022/day9/9.rb
@@ -31,6 +31,7 @@ def print_board(board)
   end
 end
 
+# for visual debugging
 def update_board(board, pos, char)
   board[pos[0]][pos[1]] = char
 end
@@ -39,18 +40,12 @@ input.each do |dir|
   dir, n = dir.split(' ')
   n.to_i.times do
     move_queue << dir
-    update_board(board, head_position, '.')
-    update_board(board, tail_position, '.')
     head_position = move(board, head_position, dir)
-    update_board(board, head_position, 'H')
-    update_board(board, tail_position, 'T')
     if head_position.zip(tail_position).map { |a, b| a - b }.any? { |n| n.abs > 1 }
-      update_board(board, tail_position, '.')
       visited << tail_position.dup
       while move_queue.size > 1
         tail_position = move(board, tail_position, move_queue.shift)
       end
-      update_board(board, tail_position, 'T')
     end
   end
 end
