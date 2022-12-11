9eaa27d9ccaa7b43397609c46d0ec24f73de54d8
Nate Dalo <ndalo@hey.com>
2022-12-11 11:53:39 -0600

    day 9 part 1 finished

--- 2022/day9/9.rb
+++ 2022/day9/9.rb
@@ -1,19 +1,17 @@
-input = open('sample.txt').read.split("\n")
+require 'pry'
+require 'pry-byebug'
 
-board = [
-  ['.','.','.','.','.','.'],
-  ['.','.','.','.','.','.'],
-  ['.','.','.','.','.','.'],
-  ['.','.','.','.','.','.'],
-  ['H','.','.','.','.','.'],
-]
+input = open('input.txt').read.split("\n")
+
+board = Array.new(500, Array.new(500, '.'))
+
+visited = []
 
 head_position = [4, 0]
 tail_position = [4, 0]
+move_queue = []
 
 def move(board, pos, dir)
-  board[pos[0]][pos[1]] = '#'
-
   case dir
   when 'U'
     pos[0] -= 1
@@ -24,7 +22,6 @@ def move(board, pos, dir)
   when 'L'
     pos[1] -= 1
   end
-  board[pos[0]][pos[1]] = 'H'
   pos
 end
 
@@ -34,24 +31,28 @@ def print_board(board)
   end
 end
 
-# interactive board
-#loop do
-#  print "Enter a direction to move (NSEW): "
-#  dir = gets.chomp
-#  head_position = move(board, head_position, dir)
-#  for row in board
-#    puts row.join
-#  end
-#end
-#
-# programmatic board
+def update_board(board, pos, char)
+  board[pos[0]][pos[1]] = char
+end
+
 input.each do |dir|
-  direction, n = dir.split(' ')
+  dir, n = dir.split(' ')
   n.to_i.times do
-    head_position = move(board, head_position, direction)
-    print_board(board)
-    puts
+    move_queue << dir
+    update_board(board, head_position, '.')
+    update_board(board, tail_position, '.')
+    head_position = move(board, head_position, dir)
+    update_board(board, head_position, 'H')
+    update_board(board, tail_position, 'T')
+    if head_position.zip(tail_position).map { |a, b| a - b }.any? { |n| n.abs > 1 }
+      update_board(board, tail_position, '.')
+      visited << tail_position.dup
+      while move_queue.size > 1
+        tail_position = move(board, tail_position, move_queue.shift)
+      end
+      update_board(board, tail_position, 'T')
+    end
   end
 end
 
-print_board(board)
+p visited.uniq.size + 1
