f672fef7b399e47f52ea6472decee8c3284582ed
Nate Dalo <ndalo@hey.com>
2025-05-24 14:01:37 -0500

    2016 day 8

---
+++ 2016/day08/day8.rb
@@ -0,0 +1,246 @@
+require "debug"
+require "awesome_print"
+
+commands = DATA.readlines(chomp: true)
+
+lcd_display = Array.new(6) { Array.new(50, ".") }
+
+class Rectangle
+  attr_reader :rows, :cols
+
+  def initialize(dimensions)
+    rows, cols = dimensions.split("x")
+    @rows = rows.to_i
+    @cols = cols.to_i
+  end
+end
+
+class Rotation
+  attr_reader :direction, :starting_position, :amount
+
+  def initialize(direction, starting_position, amount)
+    @direction = direction
+    _axis, position = starting_position.scan(/^(\w+)=(\d+)$/).flatten
+    @starting_position = position.to_i
+    @amount = amount.to_i
+  end
+end
+
+class CommandFactory
+  def self.parse(string)
+    string = string.split(" ")
+
+    case string
+    in ["rect", dimensions]
+      Rectangle.new(dimensions)
+    in ["rotate", dir, starting_position, _, amount]
+      Rotation.new(dir, starting_position, amount)
+    end
+  end
+end
+
+commands = commands.map { |command| CommandFactory.parse(command) }
+
+commands.each do |command|
+  if command.is_a?(Rectangle)
+    command.cols.times do |col_idx|
+      command.rows.times do |row_idx|
+        lcd_display[col_idx][row_idx] = "█"
+      end
+    end
+  else
+    if command.direction == "column"
+      new_column = lcd_display.transpose.dig(command.starting_position)
+      new_column = new_column.rotate(-command.amount)
+      lcd_display = lcd_display.transpose
+      lcd_display[command.starting_position] = new_column
+      lcd_display = lcd_display.transpose
+    else
+      new_row = lcd_display.dig(command.starting_position)
+      new_row = new_row.rotate(-command.amount)
+      lcd_display[command.starting_position] = new_row
+    end
+  end
+end
+
+puts lcd_display.flatten.count("█")
+
+# part 2 zoom out if necessary to read characters: AFBUPZBJPS
+
+lcd_display.each do |row|
+  p row.join
+end
+__END__
+rect 1x1
+rotate row y=0 by 20
+rect 1x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 3
+rect 2x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 3
+rect 2x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 4
+rect 2x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 3
+rect 2x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 5
+rect 1x1
+rotate row y=0 by 2
+rect 1x1
+rotate row y=0 by 6
+rect 5x1
+rotate row y=0 by 2
+rect 1x3
+rotate row y=2 by 8
+rotate row y=0 by 8
+rotate column x=0 by 1
+rect 7x1
+rotate row y=2 by 24
+rotate row y=0 by 20
+rotate column x=5 by 1
+rotate column x=4 by 2
+rotate column x=2 by 2
+rotate column x=0 by 1
+rect 7x1
+rotate column x=34 by 2
+rotate column x=22 by 1
+rotate column x=15 by 1
+rotate row y=2 by 18
+rotate row y=0 by 12
+rotate column x=8 by 2
+rotate column x=7 by 1
+rotate column x=5 by 2
+rotate column x=2 by 1
+rotate column x=0 by 1
+rect 9x1
+rotate row y=3 by 28
+rotate row y=1 by 28
+rotate row y=0 by 20
+rotate column x=18 by 1
+rotate column x=15 by 1
+rotate column x=14 by 1
+rotate column x=13 by 1
+rotate column x=12 by 2
+rotate column x=10 by 3
+rotate column x=8 by 1
+rotate column x=7 by 2
+rotate column x=6 by 1
+rotate column x=5 by 1
+rotate column x=3 by 1
+rotate column x=2 by 2
+rotate column x=0 by 1
+rect 19x1
+rotate column x=34 by 2
+rotate column x=24 by 1
+rotate column x=23 by 1
+rotate column x=14 by 1
+rotate column x=9 by 2
+rotate column x=4 by 2
+rotate row y=3 by 5
+rotate row y=2 by 3
+rotate row y=1 by 7
+rotate row y=0 by 5
+rotate column x=0 by 2
+rect 3x2
+rotate column x=16 by 2
+rotate row y=3 by 27
+rotate row y=2 by 5
+rotate row y=0 by 20
+rotate column x=8 by 2
+rotate column x=7 by 1
+rotate column x=5 by 1
+rotate column x=3 by 3
+rotate column x=2 by 1
+rotate column x=1 by 2
+rotate column x=0 by 1
+rect 9x1
+rotate row y=4 by 42
+rotate row y=3 by 40
+rotate row y=1 by 30
+rotate row y=0 by 40
+rotate column x=37 by 2
+rotate column x=36 by 3
+rotate column x=35 by 1
+rotate column x=33 by 1
+rotate column x=32 by 1
+rotate column x=31 by 3
+rotate column x=30 by 1
+rotate column x=28 by 1
+rotate column x=27 by 1
+rotate column x=25 by 1
+rotate column x=23 by 3
+rotate column x=22 by 1
+rotate column x=21 by 1
+rotate column x=20 by 1
+rotate column x=18 by 1
+rotate column x=17 by 1
+rotate column x=16 by 3
+rotate column x=15 by 1
+rotate column x=13 by 1
+rotate column x=12 by 1
+rotate column x=11 by 2
+rotate column x=10 by 1
+rotate column x=8 by 1
+rotate column x=7 by 2
+rotate column x=5 by 1
+rotate column x=3 by 3
+rotate column x=2 by 1
+rotate column x=1 by 1
+rotate column x=0 by 1
+rect 39x1
+rotate column x=44 by 2
+rotate column x=42 by 2
+rotate column x=35 by 5
+rotate column x=34 by 2
+rotate column x=32 by 2
+rotate column x=29 by 2
+rotate column x=25 by 5
+rotate column x=24 by 2
+rotate column x=19 by 2
+rotate column x=15 by 4
+rotate column x=14 by 2
+rotate column x=12 by 3
+rotate column x=9 by 2
+rotate column x=5 by 5
+rotate column x=4 by 2
+rotate row y=5 by 5
+rotate row y=4 by 38
+rotate row y=3 by 10
+rotate row y=2 by 46
+rotate row y=1 by 10
+rotate column x=48 by 4
+rotate column x=47 by 3
+rotate column x=46 by 3
+rotate column x=45 by 1
+rotate column x=43 by 1
+rotate column x=37 by 5
+rotate column x=36 by 5
+rotate column x=35 by 4
+rotate column x=33 by 1
+rotate column x=32 by 5
+rotate column x=31 by 5
+rotate column x=28 by 5
+rotate column x=27 by 5
+rotate column x=26 by 3
+rotate column x=25 by 4
+rotate column x=23 by 1
+rotate column x=17 by 5
+rotate column x=16 by 5
+rotate column x=13 by 1
+rotate column x=12 by 5
+rotate column x=11 by 5
+rotate column x=3 by 1
+rotate column x=0 by 1
