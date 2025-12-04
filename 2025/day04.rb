e99a1e1ad68312da4c84b3390b0fdf25aa069b6a
Nate Dalo <ndalo@hey.com>
2025-12-04 08:56:14 -0600

    2025 day 4 part 2

--- 2025/day04.rb
+++ 2025/day04.rb
@@ -4,18 +4,20 @@ require "debug"
 require_relative "../utils.rb"
 
 raw = AOC::Input.resolve(ARGV, DATA)
+
+# io.readlines(chomp: true).map(&:chars)
 grid = AOC::Parser.graph(raw)
 
 class Grid
   attr_reader :grid
-  attr_accessor :paper_count
 
   def initialize(grid)
     @grid = grid
-    @paper_count = 0
   end
 
   def paper_count
+    paper_count = 0
+
     grid.each_with_index do |row, rix|
       row.each_with_index do |col, cix|
         next unless col == "@"
@@ -28,11 +30,43 @@ class Grid
             end
           end
         end
-        @paper_count = @paper_count + 1 if count < 4
+        paper_count = paper_count + 1 if count < 4
       end
     end
 
-    @paper_count
+    paper_count
+  end
+
+  def paper_count_part_two
+    paper_count = 0
+
+    loop do
+      paper_removed = 0
+      grid.each_with_index do |row, rix|
+        row.each_with_index do |col, cix|
+          next unless col == "@"
+
+          count = 0
+          AOC::DIRECTIONS.each do |dy, dx|
+            if in_bounds?(rix + dy, cix + dx)
+              if grid[rix + dy][cix + dx] == "@"
+                count += 1
+              end
+            end
+          end
+
+          if count < 4
+            paper_count = paper_count + 1
+            grid[rix][cix] = "."
+            paper_removed += 1
+          end
+        end
+      end
+
+      break if paper_removed.zero?
+    end
+
+    paper_count
   end
 
   private
@@ -46,6 +80,7 @@ end
 
 g = Grid.new(grid)
 puts g.paper_count
+puts g.paper_count_part_two
 
 __END__
 ..@@.@@@@.
