29714be8f1530dfd3ef7167ca869b0fdada43e13
Nate Dalo <ndalo@hey.com>
2025-12-06 09:40:41 -0600

    2025 day 6 part 2

--- 2025/day06.rb
+++ 2025/day06.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
 require "debug"
-require "ostruct"
 require_relative "../utils.rb"
 
 raw = AOC::Input.resolve(ARGV, DATA)
-data = AOC::Parser.raw_data(raw).split("\n").map { _1.split(" ") }.transpose
+data = AOC::Parser.raw_data(raw).split("\n")
+
+part_one_data = data.map { _1.split(" ") }.transpose
+part_two_data = data.map(&:chars).transpose.slice_before { _1.all?(" ") }
 
 PartOne = Struct.new(:problems) do
   def numbers
@@ -21,9 +23,25 @@ PartOne = Struct.new(:problems) do
   end
 end
 
-part_one = data.map { PartOne.new(_1) }
+PartTwo = Struct.new(:problems) do
+  def numbers
+    problems.map { |arr| arr[..-2] }.map(&:join).map(&:to_i).reject(&:zero?)
+  end
+
+  def operand
+    problems.flatten.detect { |char| %w[* +].include?(char) }
+  end
+
+  def solution
+    numbers.reduce(operand)
+  end
+end
+
+part_one = part_one_data.map { PartOne.new(_1) }
+part_two = part_two_data.map { PartTwo.new(_1) }
 
 puts part_one.map { _1.solution }.sum
+puts part_two.map { _1.solution }.sum
 __END__
 123 328  51 64 
  45 64  387 23 
