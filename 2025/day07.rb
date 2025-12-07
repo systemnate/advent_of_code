df2ca510bf1836e206c05ea3b83dd353c0cb661c
Nate Dalo <ndalo@hey.com>
2025-12-07 16:59:29 -0600

    2025 part 7 (dfs)

--- 2025/day07.rb
+++ 2025/day07.rb
@@ -2,68 +2,42 @@ require "set"
 require_relative "../utils.rb"
 
 raw  = AOC::Input.resolve(ARGV, DATA)
-data = AOC::Parser.graph(raw)
-rows = data.size
-cols = data.first.size
-
-graph = {}
+grid = AOC::Parser.graph(raw)
+height = grid.size
 start_row = start_col = nil
 
-data.each_with_index do |row, r|
-  row.each_with_index do |ch, c|
-    case ch
-    when "^"
-      graph[[r, c]] = 0
-    when "S"
-      start_row, start_col = r, c
-    end
-  end
-end
-
-first_split = nil
-r = start_row + 1
-while r < rows
-  if data[r][start_col] == "^"
-    first_split = [r, start_col]
+grid.each_with_index do |row, r|
+  if (c = row.index("S"))
+    start_row, start_col = r, c
     break
   end
-  r += 1
 end
 
-graph[first_split] = 1
+seen  = Set.new
+stack = [[start_row, start_col]]
 
-graph.keys.each do |row, col|
-  next unless graph[[row, col]] == 1
+until stack.empty?
+  beam_row, beam_col = stack.pop
+  r = beam_row + 1
 
-  # left
-  if col - 1 >= 0
-    r = row + 1
-    while r < rows
-      if data[r][col - 1] == "^"
-        left = [r, col - 1]
-        graph[left] = 1 if graph[left] == 0
-        break
-      end
-      r += 1
+  while r < height
+    if grid[r][beam_col] == "^"
+      pos = [r, beam_col]
+      break if seen.include?(pos)
+
+      seen << pos
+
+      stack << [r, beam_col - 1]
+      stack << [r, beam_col + 1]
+
+      break
     end
-  end
 
-  # right
-  if col + 1 < cols
-    r = row + 1
-    while r < rows
-      if data[r][col + 1] == "^"
-        right = [r, col + 1]
-        graph[right] = 1 if graph[right] == 0
-        break
-      end
-      r += 1
-    end
+    r += 1
   end
 end
 
-puts graph.count { |_k, v| v == 1 }
-
+puts seen.size
 __END__
 .......S.......
 ...............
